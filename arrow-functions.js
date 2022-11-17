78a03db0d50b71523881171d1456f40a3a398ffd
James McBeath <jamesmcbeath@me.com>
2022-11-17 19:56:46 +0000

    if applied, inserted code for the exercise

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,29 @@
  */
 
 // Vanilla JavaScript Function
+const addTwoNumbers = (a, b) => {
+//code block is between curly braces
+    return a + b;
+    }
+    let sum = addTwoNumbers(3, 5);
 
+    console.log(sum);
 // Arrow Function With Parameters
-
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2 (6, 4);
+console.log(sum2);
 // Single Line Arrow Function With Parameters
-
+const sayName = message => console.log(message);
+sayName('James');
 // Implicit Returns
-
+const sayHello = () => console.log('hello');
+sayHello()
 // Returning Multiple Lines
+const returnAddress = () => (
+    `<p>
+    This is a multiline string
+    42 Feldspar Close
+    Kent
+    </p>`
+)
+console.log(returnAddress());
